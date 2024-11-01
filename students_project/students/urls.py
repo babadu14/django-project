269390025ade529fc528394d2745496bcaf7f9a4
babadu14 <andriamanagadze7@gmail.com>
2024-11-02 01:33:51 +0400

    previous project just added debug toolbar

--- students_project/students/urls.py
+++ students_project/students/urls.py
@@ -3,5 +3,5 @@ from .views import main_page_view, students_page_view
 
 urlpatterns =[
     path('main/', main_page_view),
-    path('students/', students_page_view)
+    path('students/', students_page_view),
 ]
